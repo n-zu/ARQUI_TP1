aedab9105db37a092a006282988b61e3b6df2334
Nicolas Zulaica <nzulaica@fi.uba.ar>
2023-04-16 14:27:56 -0300

    add port to docker compose

--- app/index.js
+++ app/index.js
@@ -1,24 +1,22 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const metar = require('./src/routes/metar');
-const space_news = require('./src/routes/space_news');
-const fact = require('./src/routes/useless_fact');
+const express = require("express");
+const dotenv = require("dotenv");
+const metar = require("./src/routes/metar");
+const space_news = require("./src/routes/space_news");
+const fact = require("./src/routes/useless_fact");
 const app = express();
 
 dotenv.config();
 
-
 // Ping route
-app.get('/ping', async (req, res) => {
-    res.send('Pong!');
+app.get("/ping", async (req, res) => {
+  res.send("Pong!");
 });
 
-app.get('/metar', metar);
-app.get('/space_news', space_news);
-app.get('/fact', fact);
-
+app.get("/metar", metar);
+app.get("/space_news", space_news);
+app.get("/fact", fact);
 
 // Start the server
 app.listen(process.env.PORT, () => {
-    console.log('Server started on port 3000');
+  console.log("Server started on port " + process.env.PORT);
 });
