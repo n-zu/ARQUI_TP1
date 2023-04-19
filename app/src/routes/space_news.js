3b7f4ebfde399fcb8f501d057e054c58a45b9f03
FScaccheri <fscaccheri@gmail.com>
2023-04-19 19:59:27 -0300

    Add redis image to docker compose. Add redis npm package and initialize it

--- app/src/routes/space_news.js
+++ app/src/routes/space_news.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const axios = require("axios");
+const redis = require('redis');
 const router = express.Router();
 
 const ARTICLES_AMOUNT = 5;
+
+const redisClient = redis.createClient({ url: 'redis://redis:6379' });
+
+(async () => {
+    await redisClient.connect();
+})();
+
+process.on('SIGTERM', async () => {
+    await redisClient.quit();
+});
 
 router.get('/space_news', async (req, res) => {
     const articles = await axios.get(`https://api.spaceflightnewsapi.net/v3/articles?_limit=${ARTICLES_AMOUNT}`);
