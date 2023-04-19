3ff6b7da5fe5ae3ebf4b133fe0072bc87cc546cc
FScaccheri <fscaccheri@gmail.com>
2023-04-19 20:40:35 -0300

    Add redis cache to space news endpoint. add flag to turn cache on or off

--- app/src/routes/space_news.js
+++ app/src/routes/space_news.js
@@ -5,6 +5,11 @@ const router = express.Router();
 
 const ARTICLES_AMOUNT = 5;
 
+const CAN_CACHE = process.env.CACHE === 'true';
+
+const SPACE_NEWS_KEY = 'space_news';
+const SPACE_NEWS_TTL = 5;
+
 const redisClient = redis.createClient({ url: 'redis://redis:6379' });
 
 (async () => {
@@ -16,8 +21,20 @@ process.on('SIGTERM', async () => {
 });
 
 router.get('/space_news', async (req, res) => {
-    const articles = await axios.get(`https://api.spaceflightnewsapi.net/v3/articles?_limit=${ARTICLES_AMOUNT}`);
-    const titles = articles.data.map(article => article.title);
+    let titles;
+    const titlesString = await redisClient.get('space_news');
+
+    if (titlesString !== null && CAN_CACHE) {
+        titles = JSON.parse(titlesString);
+    } else {
+        const articles = await axios.get(`https://api.spaceflightnewsapi.net/v3/articles?_limit=${ARTICLES_AMOUNT}`);
+        titles = articles.data.map(article => article.title);
+
+        await redisClient.set(SPACE_NEWS_KEY, JSON.stringify(titles), {
+            EX: SPACE_NEWS_TTL
+        });
+    }
+    
     res.status(200).send(titles);
 });
 
