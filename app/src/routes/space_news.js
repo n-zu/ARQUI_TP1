59061487ea969969e9e00007fc8288bc774a6ec2
efoppiano <efoppiano@fi.uba.ar>
2023-04-22 22:06:55 -0300

    WIP: configure hot-shots for space_news and add new panel to Grafana

--- app/src/routes/space_news.js
+++ app/src/routes/space_news.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const axios = require("axios");
 const router = express.Router();
+const Client = require('hot-shots');
 
 const ARTICLES_AMOUNT = 5;
 
+const client = new Client({
+    host: 'graphite',
+    port: 8125,
+    prefix: 'external_api_',
+});
+
 router.get('/space_news', async (req, res) => {
+    const start = process.hrtime();
     const articles = await axios.get(`https://api.spaceflightnewsapi.net/v3/articles?_limit=${ARTICLES_AMOUNT}`);
+    const end = process.hrtime(start);
+    const duration = end[0] * 1e3 + end[1] * 1e-6;
+
+    client.gauge('space_news', duration);
+
     const titles = articles.data.map(article => article.title);
     res.status(200).send(titles);
 });
