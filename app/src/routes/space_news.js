26d0bf6b3a8c17ba6e896dffed106d8aca251347
FScaccheri <fscaccheri@gmail.com>
2023-04-25 20:14:52 -0300

    Move Redis connection logic to new space_news service

--- app/src/routes/space_news.js
+++ app/src/routes/space_news.js
@@ -1,38 +1,16 @@
 const express = require('express');
-const axios = require("axios");
-const redis = require('redis');
 const router = express.Router();
-
-const ARTICLES_AMOUNT = 5;
+const service = require('../services/space_news');
 
 const CAN_CACHE = process.env.CACHE === 'true';
 
-const SPACE_NEWS_KEY = 'space_news';
-const SPACE_NEWS_TTL = 5;
-
-const redisClient = redis.createClient({ url: 'redis://redis:6379' });
-
-(async () => {
-    await redisClient.connect();
-})();
-
-process.on('SIGTERM', async () => {
-    await redisClient.quit();
-});
-
 router.get('/space_news', async (req, res) => {
     let titles;
-    const titlesString = await redisClient.get('space_news');
 
-    if (titlesString !== null && CAN_CACHE) {
-        titles = JSON.parse(titlesString);
+    if (CAN_CACHE) {
+        titles = await service.fetchFromCache();
     } else {
-        const articles = await axios.get(`https://api.spaceflightnewsapi.net/v3/articles?_limit=${ARTICLES_AMOUNT}`);
-        titles = articles.data.map(article => article.title);
-
-        await redisClient.set(SPACE_NEWS_KEY, JSON.stringify(titles), {
-            EX: SPACE_NEWS_TTL
-        });
+        titles = await service.fetchNews(ARTICLES_AMOUNT);     
     }
     
     res.status(200).send(titles);
---
+++ app/src/services/space_news.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const redis = require('redis');
+
+const ARTICLES_AMOUNT = 5;
+const SPACE_NEWS_KEY = 'space_news';
+const SPACE_NEWS_TTL = 5;
+
+const CAN_CACHE = process.env.CACHE === 'true';
+
+let redisClient = null;
+
+if (CAN_CACHE) {
+    redisClient = redis.createClient({ url: 'redis://redis:6379' });
+    (async () => {
+        await redisClient.connect();
+    })();
+    
+    process.on('SIGTERM', async () => {
+        await redisClient.quit();
+    });
+}
+
+export async function fetchNews() {
+    const articles = await axios.get(`https://api.spaceflightnewsapi.net/v3/articles?_limit=${ARTICLES_AMOUNT}`);
+    return articles.data.map(article => article.title);
+}
+
+export async function fetchFromCache() {
+     if (!redisClient) return await utils.fetchNews();
+
+    const titlesString = await redisClient.get(SPACE_NEWS_KEY);
+    if (titlesString !== null) {
+        return JSON.parse(titlesString);
+    } else {
+        const titles = await utils.fetchNews();
+
+        await redisClient.set(SPACE_NEWS_KEY, JSON.stringify(titles), {
+            EX: SPACE_NEWS_TTL
+        });
+
+        return titles;
+    }
+}
