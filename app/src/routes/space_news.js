b0d7f5d02d749b9ac6e072aa312673ffd89b3dff
efoppiano <efoppiano@fi.uba.ar>
2023-05-05 13:58:24 -0300

    Minor fix in space_news endpoint

--- app/src/routes/space_news.js
+++ app/src/routes/space_news.js
@@ -9,7 +9,7 @@ const metricsLogger = new MetricsLogger('space_news');
 
 router.get('/space_news', async (req, res) => {
     const articles = await metricsLogger.runAndMeasure(async () => {
-        return axios.get(`https://api.spaceflightnewsapi.net/v3/articles?_limit=${ARTICLES_AMOUNT}`);
+        return await axios.get(`https://api.spaceflightnewsapi.net/v3/articles?_limit=${ARTICLES_AMOUNT}`);
     });
 
     const titles = articles.data.map(article => article.title);
