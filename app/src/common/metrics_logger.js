343a5a4775788835c967b9776f9911cc317863d7
efoppiano <efoppiano@fi.uba.ar>
2023-05-07 22:33:00 -0300

    Change MetricsLogger to only push duration, and update dashboard to use '' variable

--- app/src/common/metrics_logger.js
+++ app/src/common/metrics_logger.js
@@ -4,22 +4,12 @@ const GRAPHITE_HOST = process.env.GRAPHITE_HOST || 'graphite';
 const GRAPHITE_PORT = process.env.GRAPHITE_PORT || 8125;
 
 class MetricsLogger {
-    constructor(serviceName, window_size = 50) {
+    constructor(serviceName) {
         this.client = new Client({
             host: GRAPHITE_HOST,
             port: GRAPHITE_PORT,
             prefix: 'external_api.' + serviceName + '.'
         });
-        this.values = [];
-        this.windowSize = window_size;
-    }
-
-    calculateAvg() {
-        return this.values.reduce((a, b) => a + b, 0) / this.values.length;
-    }
-
-    calculateMax() {
-        return Math.max(...this.values);
     }
 
     log(metric_name, value) {
@@ -32,16 +22,7 @@ class MetricsLogger {
         const end = process.hrtime(start);
         const duration = end[0] * 1e3 + end[1] * 1e-6;
 
-        if (this.values.length >= this.windowSize) {
-            this.values.shift();
-        }
-        this.values.push(duration);
-
-        const avg = this.calculateAvg();
-        const max = this.calculateMax();
-
-        this.log('avg', avg);
-        this.log('max', max);
+        this.log('delay', duration);
 
         return result;
     }
